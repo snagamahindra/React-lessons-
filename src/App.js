8ac8499fc7ddcd2ad2da044619fee30d750bcc2e
S.N.Mahendra <snagamahindra@gmail.com>
2024-01-17 13:03:58 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const doubleIncrement = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const doubleDecrement = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
+  const restart = () => {
+    setCounter(0);
+  };
+
+  const infinity = () => {
+    setCounter(1 / 0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+      <div className="d-flex">
+        <div className="p-2">
+          <button className="btn btn-primary" onClick={increment}>
+            Increment
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="btn btn-dark" onClick={decrement}>
+            Decrement
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="btn btn-success" onClick={doubleIncrement}>
+            Double Increment
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="btn btn-danger" onClick={doubleDecrement}>
+            Double Decrement
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="btn btn-warning" onClick={restart}>
+            Restart
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="btn btn-secondary" onClick={infinity}>
+            Infinity
+          </button>
+        </div>
+      </div>
+      <div className="display-2">{counter}</div>
     </div>
   );
 }
